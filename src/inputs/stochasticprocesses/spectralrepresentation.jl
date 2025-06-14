30f194843c949d9e555cf5c6acd1bab0e63a4202
Mira253 <mira.schmidt@stud.uni-hannover.de>
2025-06-14 10:11:05 +0200

    Changed the Nyquist Frequency check to only warning

--- src/inputs/stochasticprocesses/spectralrepresentation.jl
+++ src/inputs/stochasticprocesses/spectralrepresentation.jl
@@ -50,9 +50,7 @@ function SpectralRepresentation(
     f_ny = f_s/2            # Nyquist Frequency
 
     if f_max > f_ny
-        error("⚠️ The frequency of the signal ($f_max Hz) is bigger than the Nyquist Frequency ($f_ny Hz). There is a risk for Aliasing!")
-    else
-        println("✅ No risk of Aliasing: f_signal_max = $f_max Hz ≤ f_Nyquist = $f_ny Hz")
+        @warn "The frequency of the signal ($f_max Hz) is bigger than the Nyquist Frequency ($f_ny Hz). There is a risk for Aliasing!"
     end
 
     A = sqrt.(2 * psd.p * Δω)
